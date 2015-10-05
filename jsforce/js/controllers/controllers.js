cfd52735c1ab3b5e566d9ab6986e1726b34f26d0
Prishan Fernando <prishanf@yahoo.com>
2015-10-05 16:28:50 -0700

    add jsforce to service file

--- jsforce/js/controllers/controllers.js
+++ jsforce/js/controllers/controllers.js
@@ -1,11 +1,17 @@
 SFDCAdminHelperControllers.
 controller('SFDCObjectController',['$rootScope','myFactory',function($rootScope,myFactory){
     self = this;
-    self.objects=[];
-    this.artist = myFactory.getArtist();
-
-    conn.describeGlobal(function(err, res) {
-        if (err) { return console.error(err); }
+    this.objs=['a'];
+    self.objects =['Init'];
+  //  this.artist = myFactory.getArtist();
+    conn.describeGlobal().then(function(err, res) {
+        self.objects.push('Err');
+        console.log(self);
+        if (err) {
+          console.log(err);
+          self.objects = ['Error'];
+          return self;
+        }
         var objNames= res.sobjects.filter(function(item){
           return item.layoutable;
         });
