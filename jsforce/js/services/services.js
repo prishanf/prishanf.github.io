a18e57d7045ee716f5108b953e2355292b1ae650
Prishan Fernando <prishanf@yahoo.com>
2015-10-05 14:43:44 -0700

    add jsforce to service file

--- jsforce/js/services/services.js
+++ jsforce/js/services/services.js
@@ -3,8 +3,22 @@ SFDCAdminHelperControllers
   var _artist = 'Shakira';
   var service = {};
 
-  service.getArtist = function(){
-    return _artist;
+  // service.getArtist = function(){
+  //   return _artist;
+  // }
+
+  service.getArtist = function() {
+    conn.describeGlobal(function(err, res) {
+        if (err) { return console.error(err); }
+        var objNames= res.sobjects.filter(function(item){
+          return item.layoutable;
+        });
+        console.log(objNames.length);
+        objNames.forEach(function(i){
+           console.log(i.label);
+        });
+        return objNames;
+      });
   }
 
   return service;
